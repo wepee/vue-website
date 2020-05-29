6e2a7b8018aae39cb5aae4263f31d749ed860a5f
Adam Aleb <adam.aleb@etu.univ-st-etienne.fr>
2020-05-29 14:34:33 +0200

    ressources fix

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+let routes;
+routes = [
   {
     path: '/',
     name: 'Home',
@@ -17,8 +18,24 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/mon-parcours',
+    name: 'parcours',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Parcours.vue')
+  },
+  {
+    path: '/ressources',
+    name: 'ressources',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Ressources.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
